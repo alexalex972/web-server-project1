a1889c45c24813d67d9d8fc0fe262c97d5a8bfef
Aleks Mishev <aleks.mishev@resolutesoftware.com>
2019-11-07 02:04:11 +0200

    .net is bad

--- inc/login_script.php
+++ inc/login_script.php
@@ -38,3 +38,17 @@ if (isset($_POST['submit'])) {
    $conn = null;
 }
 ?>
+<form action="" method="post">
+   <div style="margin: 0 auto; max-width: 500px; margin-top: 50px; margin-bottom: 5px;">
+      <table class="form-group">
+         <label>Email :</label>
+         <input id="name" class="form-control" name="email" placeholder="john.doe@email.com" type="text">
+         <label style="margin-top: .5rem;">Password :</label>
+         <input id="password" style="margin-bottom: 1rem;" class="form-control" name="password" placeholder="**********" type="password">
+         <div align="center"><input name="submit" class="btn btn-secondary" type="submit" value="Login"></div>
+         <span><?php
+               echo $error;
+               ?></span>
+      </table>
+   </div>
+</form>
